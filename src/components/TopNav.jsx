0396a5eecef90b50db98fa1ae9ec3db24315ef1f
nomayen <nomayen.ohin@gmail.com>
2023-10-15 09:45:58 +0600

    first commit

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TopNav = () => {
+    return (
+        <div>
+            <div className="navbar bg-blue-600 text-neutral-content">
+               <p className='text-white'>Lorem ipsum dolor sit amet consectetora nam eos.</p>
+            </div>
+        </div>
+    );
+};
+
+export default TopNav;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import TopNav from "../../components/TopNav";
+
+const Home = () => {
+    return (
+        <>
+        <TopNav></TopNav>
+        
+        </>
+    );
+};
+
+export default Home;
